f94aad00dc3f6e77c6c95aba9f0a56bb3b72c9a0
natika1 <natika1@g.pl>
2020-04-20 12:35:45 +0200

    Revert "dodałam komenta"
    This reverts commit 8af03aaa4789b5a19f65a80cd0318b8f15204ced.

--- main/java/com/example/androidlab/MainActivity.java
+++ main/java/com/example/androidlab/MainActivity.java
@@ -8,7 +8,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //to do lalala
+        //to do lalala alal
+        //dodałam komenta
+        //jakies dodatkowe zmiany
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
