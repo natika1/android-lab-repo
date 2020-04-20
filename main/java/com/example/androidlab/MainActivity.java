51c1e8ae63c9ceeded1ea46a15e5253776d8f0e7
natika1 <natika1@g.pl>
2020-04-20 11:44:27 +0200

    Initial commit

---
+++ main/java/com/example/androidlab/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //to do lalala
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
